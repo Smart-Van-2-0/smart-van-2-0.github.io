facdb0e6896f6a02615842c9214e579b4160eb89
robypomper <robypomper@johnosproject.org>
2023-08-24 15:53:38 +0200

    feat(comps): Added WebSiteMap component to use to show the website's main links

---
+++ src/components/WebSiteMap/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import styles from './styles.module.css';
+
+export function MapDocs() {
+  return (<a href="/docs">DOCS</a>);
+}
+
+export function MapFeatures() {
+  return (<a href="/docs/features">FEATURES</a>);
+}
+
+export function MapHardware() {
+  return (<a href="/docs/hardware">HARDWARE</a>);
+}
+
+export function MapSoftware() {
+  return (<a href="/docs/software">SOFTWARE</a>);
+}
+
+export function MapAbout() {
+  return (<a href="/about">ABOUT</a>);
+}
+
+export function MapCollaborate() {
+  return (<a href="/collaborate">COLLABORATE</a>);
+}
+
+export function MapRoadmap() {
+  return (<a href="/roadmap">ROADMAP</a>);
+}
+
+
+export default function WebSiteMap() {
+  return (
+    <div className={styles.web_site_map}>
+      <MapFeatures />
+      |
+      <MapHardware />
+      |
+      <MapSoftware />
+      |
+      <MapAbout />
+      |
+      <MapCollaborate />
+      |
+      <MapRoadmap />
+    </div>
+  )
+}
