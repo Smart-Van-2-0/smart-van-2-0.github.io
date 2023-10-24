a085b737aa32d20b293b53fc74f59a10f5ec0eb3
robypomper <robypomper@johnosproject.org>
2023-10-24 16:36:48 +0200

    feat(All): Added the GTag for Google Analytics

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,6 +44,10 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        gtag: {
+          trackingID: 'G-HSBHLDVTYK',
+          anonymizeIP: true,
+        }
       }),
     ],
   ],
